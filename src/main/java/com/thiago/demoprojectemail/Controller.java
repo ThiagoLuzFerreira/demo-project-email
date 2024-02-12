68d543ea83525043acf8e48cb11baf29332b768b
ThiagoLuzFerreira <tlrferreira83@gmail.com>
2024-02-12 18:39:39 -0300

    configurations for config client added

--- src/main/java/com/thiago/demoprojectemail/Controller.java
+++ src/main/java/com/thiago/demoprojectemail/Controller.java
@@ -1,5 +1,6 @@
 package com.thiago.demoprojectemail;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/email")
 public class Controller {
 
+    @Value("${file.test}")
+    private String config;
+
     @GetMapping
     public String status(){
         return "ok";
+    }
+
+    @GetMapping("/configs")
+    public String getConfig(){
+        return config;
     }
 }
